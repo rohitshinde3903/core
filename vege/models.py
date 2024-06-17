f49537f767301227aa75250a88b81b44097504ac
rohitshinde3903 <rohitshinde3903@gmail.com>
2024-06-17 20:44:45 +0530

    Adding Reciepes

--- vege/models.py
+++ vege/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
-class receipe(models.Model):
+class Receipe(models.Model):
     receipe_name = models.CharField( max_length=500)
     receipe_description = models.TextField()
     receipe_image = models.ImageField(upload_to="receipe")
--- vege/views.py
+++ vege/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 def receipes(request):
@@ -6,7 +7,18 @@ def receipes(request):
         data = request.POST
         receipe_name = data.get("receipe_name")
         receipe_description = data.get("receipe_description")
+        receipe_image =  request.FILES.get('receipe_image')
         
-        print(receipe_name)
-        print(receipe_description)
-    return render(request, "receipes.html")
+        Receipe.objects.create(
+            receipe_image = receipe_image,
+            receipe_description = receipe_description,
+            receipe_name = receipe_name
+            
+        )
+        return redirect('/receipe/')
+    
+    queryset = Receipe.objects.all()
+    context = {'receipes': queryset}
+        
+        
+    return render(request, "receipes.html", context)
